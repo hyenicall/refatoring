4c2baeb5176754a019a8411a516f94b85070488d
jihye <wisdom_u@naver.com>
2022-07-08 14:21:47 +0900

    6.3 함수 추출하기
    - 수업내용

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,27 +1,25 @@
 export function printOwing(invoice) {
+  printBanner()
+  let outstanding = calculateOutstanding(invoice)
+  recordDueDate(invoice)
+  printDetails(invoice, outstanding)
+}
+
+const printBanner = () => {
   console.log('***********************')
   console.log('**** Customer Owes ****')
   console.log('***********************')
-
-  const outstanding = calculateOutstanding(invoice)
-  checkDueDate(invoice)
-  checkLog(invoice, outstanding)
-}
-
-const invoice = {
-  orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
 }
 
 const calculateOutstanding = (invoice) => {
-  let outstanding = 0
+  let result = 0
   for (const o of invoice.orders) {
-    outstanding += o.amount
+    result += o.amount
   }
-  return outstanding
+  return result
 }
 
-const checkDueDate = (invoice) => {
+const recordDueDate = (invoice) => {
   const today = new Date()
   invoice.dueDate = new Date(
     today.getFullYear(),
@@ -30,10 +28,15 @@ const checkDueDate = (invoice) => {
   )
 }
 
-const checkLog = (invoice, outstanding) => {
+const printDetails = (invoice, outstanding) => {
   console.log(`name: ${invoice.customer}`)
   console.log(`amount: ${outstanding}`)
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
+}
+
+const invoice = {
+  orders: [{ amount: 2 }, { amount: 5 }],
+  customer: '엘리',
 }
 
 printOwing(invoice)
