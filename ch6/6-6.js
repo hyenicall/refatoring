b293e2afe71674ae7a376696127e9d80da134339
jihye <wisdom_u@naver.com>
2022-07-11 14:53:46 +0900

    6.14 변수 캡슐화하기1
    - 얕은 복사로 복사된 객제 전달하기

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,5 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+let defaultOwner = { firstName: '마틴', lastName: '파울러' }
 
 export function getDefaultOwner() {
-  return defaultOwner;
+  return { ...defaultOwner } //얕은 복사( 중첩 객체라면 중첩된 객체는 주소값이 전달되기 때문에 수정이 가능해 진다)
 }
