2e45ee3910fde1c8f9fd93562ed686b2479f179e
jihye <wisdom_u@naver.com>
2022-07-12 14:56:03 +0900

    6.19 단계 쪼개기 챌린지
    - 내가한것

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,30 +1,42 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  const price =
+    basePrice(product, quantity) -
+    discount(product, quantity) +
+    shippingCost(product, quantity, shippingMethod)
+
+  return price
 }
 
-// 사용 예:
 const product = {
   basePrice: 10,
   discountRate: 0.1,
   discountThreshold: 10,
-};
+}
 
 const shippingMethod = {
   discountThreshold: 20,
   feePerCase: 5,
   discountedFee: 3,
-};
+}
 
-const price = priceOrder(product, 5, shippingMethod);
-console.log(price);
+function basePrice(product, quantity) {
+  return product.basePrice * quantity
+}
+function discount(product, quantity) {
+  return (
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate
+  )
+}
+function shippingPerCase(product, quantity, shippingMethod) {
+  return basePrice(product, quantity) > shippingMethod.discountThreshold
+    ? shippingMethod.discountedFee
+    : shippingMethod.feePerCase
+}
+function shippingCost(product, quantity, shippingMethod) {
+  return quantity * shippingPerCase(product, quantity, shippingMethod)
+}
+
+const price = priceOrder(product, 5, shippingMethod)
+console.log(price)
