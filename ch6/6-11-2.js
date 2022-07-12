4d9e03049424c5cd49085c9d8d3992ff0db8a67c
jihye <wisdom_u@naver.com>
2022-07-12 18:18:52 +0900

    6.21 단계 쪼개기 챌린지
    - 수업내용

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,34 @@
-import fs from 'fs';
+import fs from 'fs'
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+//1. run 함수를 만들어서 노드의 process디펜던시를 제거함
+run(process.argv)
+
+//2. 사용자에게 입력을 받아옴 -> 유효성 검사
+function run(args) {
+  countOrders(parseCommand(args))
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function parseCommand(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요')
+  }
+
+  const fileName = `./${args[2]}.json`
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다')
+  }
+  const countReadOnly = args.includes('-r')
+  return {
+    fileName,
+    countReadOnly,
+  }
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countOrders(command) {
+  const rawData = fs.readFileSync(fileName)
+  const orders = JSON.parse(rawData)
+  const filtered = command.countReadOnly
+    ? orders.filter((order) => order.status === 'ready')
+    : orders
+  console.log(filtered.length)
 }
