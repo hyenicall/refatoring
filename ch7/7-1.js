c6930aee6cd60e8b209c658d1aa84312ee070faa
jihye <wisdom_u@naver.com>
2022-07-25 14:48:00 +0900

    7.3 레코드 캡슐화하기
    수업내용

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,10 +1,13 @@
 class Organization {
+  #rawData
   #name
   #country
 
-  constructor(name, country) {
-    this.#name = name
-    this.#country = country
+  constructor(data) {
+    //주로 서버와 통신할 때 json형식의 데이터가 넘어오기 때문에 data로 받고 풀어서 사용하기도 함
+    this.#rawData = data //raw데이터를 보관하고 싶다면
+    this.#name = data.name
+    this.#country = data.country
   }
 
   get name() {
@@ -18,6 +21,9 @@ class Organization {
   }
   set country(value) {
     this.#country = value
+  }
+  get rawData() {
+    return { ...this.#data } //데이터를 넘길때 얕은복사를 이용
   }
 }
 
