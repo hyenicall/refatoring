99037e75a1d30be15981cc5604b615cc3dad26a3
jihye <wisdom_u@naver.com>
2022-07-25 16:22:15 +0900

    7.5 기본형을 객체로 바꾸기

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,6 +1,37 @@
 export class Order {
+  #priority
   constructor(data) {
-    this.priority = data.priority;
+    this.#priority = new Priority(data.Priority)
+  }
+
+  isHighPriority() {
+    return this.priority.higherThan('nomal')
+  }
+}
+
+class Priority {
+  #value
+  #index
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value
+    } else {
+      throw new Error(`${value} is invalid for Priority`)
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().indexOf(this.#value)
+  }
+
+  equals(other) {
+    return this.#index > other.index
+  }
+  higherThan(other) {
+    return this.#index > other.index
+  }
+  static legalValues() {
+    return ['low', 'nomal', 'high', 'rush']
   }
 }
 
@@ -8,8 +39,7 @@ const orders = [
   new Order({ priority: 'normal' }),
   new Order({ priority: 'high' }),
   new Order({ priority: 'rush' }),
-];
+]
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPriority()).length
+console.log(highPriorityCount)
