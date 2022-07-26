767545449bab4bbfc0a4b4c52b8ac3f88778596a
jihye <wisdom_u@naver.com>
2022-07-26 17:40:54 +0900

    7.10 중개자 제거하기

--- ch7/7-7.js
+++ ch7/7-7.js
@@ -1,38 +1,15 @@
 class Person {
   #name
-  #department
-  constructor(name, department) {
+  #manager
+  #chargeCode
+  constructor(name, manager, chargeCode) {
     this.#name = name
-    this.#department = department
+    this.#manager = manager
+    this.#chargeCode = chargeCode
   }
 
   get name() {
     return this.#name
-  }
-
-  get department() {
-    return this.#department
-  }
-
-  set department(arg) {
-    this.#department = arg
-  }
-
-  get manager() {
-    return this.#department.manager
-  }
-
-  get chargeCode() {
-    return this.#department.chargeCode
-  }
-}
-
-export class Department {
-  #manager
-  #chargeCode
-  constructor(manager, chargeCode) {
-    this.#manager = manager
-    this.#chargeCode = chargeCode
   }
 
   get chargeCode() {
@@ -52,7 +29,7 @@ export class Department {
   }
 }
 
-const person = new Person('Tom', new Department('aManager', '999'))
+const person = new Person('Tom', 'aManager', '999')
 console.log(person.name)
 console.log(person.manager)
 console.log(person.chargeCode)
