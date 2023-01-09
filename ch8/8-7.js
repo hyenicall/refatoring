32c11e25c299a62b0bf47a4b0f0277dfc7d7cf37
jihye <wisdom_u@naver.com>
2023-01-09 15:38:02 +0900

    8.8 반복문 쪼개기 (책 8.7)

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -1,10 +1,11 @@
 export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
+  function youngest() {
+    return Math.min(...people.map((p) => p.age))
   }
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+  function totalSalary() {
+    return people.reduce((total, p) => (total += p.salary), 0)
+  }
+
+  return `youngestAge: ${youngest()}, totalSalary: ${totalSalary()}`
 }
