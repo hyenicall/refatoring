b27822e3223f69baaa00f5e905d1794084b0984e
jihye <wisdom_u@naver.com>
2023-01-09 15:24:32 +0900

    8.7 문장 슬라이드 하기(책 8.6)

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,18 +1,15 @@
 // 예제 1
-const pricingPlan = retrievePricingPlan();
-const order = retreiveOrder();
-let charge;
-const chargePerUnit = pricingPlan.unit;
+const pricingPlan = retrievePricingPlan()
+const chargePerUnit = pricingPlan.unit
+const order = retreiveOrder()
+let charge
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
-  return result;
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop()
+  allocatedResources.push(result)
+  return result
 }
