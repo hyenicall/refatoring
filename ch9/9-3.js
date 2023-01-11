4a9a95bed3af579c3bbf7c32b2f35b49c9c1c187
jihye <wisdom_u@naver.com>
2023-01-11 14:32:03 +0900

    9.4 파생변수를 질의 함수로 바꾸기 (책9.3)

--- ch9/9-3.js
+++ ch9/9-3.js
@@ -2,12 +2,13 @@
 class Order {
   // 다른 코드 있다고 가정
   get discountedTotal() {
-    return this._discountedTotal;
+    return this._basePrice - this.discount
   }
   set discount(value) {
-    const old = this._discount;
-    this._discount = value;
-    this._discountedTotal += old - value;
+    // 기존 : discount와 discountedTotal이 같이 update됨(스파게티코드)
+    // const old = this._discount
+    this._discount = value
+    // this._discountedTotal += old - value
   }
 }
 
@@ -15,10 +16,10 @@ class Order {
 class ProductionPlan {
   // 다른 코드 있다고 가정
   get production() {
-    return this._production;
+    return this._production.reduce((sum, a) => sum + a.amount, 0)
   }
   applyAdjustment(adjustment) {
-    this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
+    this._adjustments.push(adjustment)
+    // this._production += adjustment.amount
   }
 }
