f06adb6579753f68a10786887b90f75c5792ed69
jihye <wisdom_u@naver.com>
2023-01-11 16:43:39 +0900

    10.2 조건문 분해하기

--- ch10/10-1.js
+++ ch10/10-1.js
@@ -1,7 +1,15 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  return isSummer() ? summerCharge() : regularCharge()
+
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd)
+  }
+
+  function summerCharge() {
+    return (charge = quantity * plan.summerRate)
+  }
+
+  function regularCharge() {
+    return (charge = quantity * plan.regularRate + plan.regularServiceCharge)
+  }
 }
