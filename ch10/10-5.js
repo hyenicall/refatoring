00a2cf99fcf6d83a31f23f248bf9a31615c32d0c
jihye <wisdom_u@naver.com>
2023-01-11 20:32:32 +0900

    10.7 특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,20 +1,56 @@
+class Hotel {
+  constructor() {
+    this.rooms = []
+  }
+  addRoom(roomNumber) {
+    this.rooms[roomNumber] = new Room(roomNumber)
+  }
+
+  emptyRoom(roomNumber) {
+    this.rooms[roomNumber] = new EmptyRoom()
+  }
+  cleanRooms() {
+    this.rooms.forEach((room) => room.clean())
+  }
+}
+class Room {
+  constructor(roomNumber) {
+    this.roomNumber = roomNumber
+  }
+  clean() {
+    console.log(`${this.roomNumber} clean!!`)
+  }
+}
+
+class EmptyRoom extends Room {
+  clean() {
+    console.log(`${this.roomNumber} empty!!`)
+  }
+}
+
+const hotel = new Hotel()
+hotel.addRoom(0)
+hotel.addRoom(1)
+hotel.cleanRooms()
+hotel.emptyRoom(0)
+hotel.cleanRooms()
 export class Site {
   constructor(customer) {
-    this._customer = customer;
+    this._customer = customer
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === 'unknown' ? new UnknownCustomer() : new Customer()
   }
 }
 
 export class Customer {
   constructor(name) {
-    this._name = name;
+    this._name = name
   }
 
   get name() {
-    return this._name;
+    return this._name
   }
 
   get billingPlan() {
@@ -30,13 +66,16 @@ export class Customer {
   }
 }
 
+class UnknownCustomer extends Customer {
+  get name() {
+    return 'occupant'
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
-  const aCustomer = site.customer;
+  const aCustomer = site.customer
   // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
-
-  return customerName;
+  customerName = aCustomer.name
+  return customerName
 }
