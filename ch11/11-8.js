e41eb5eddfd86bc674ce4715bc9822a53d5ac318
jihye <wisdom_u@naver.com>
2023-01-16 15:51:18 +0900

    11.9 생성자를 팩터리 함수로 바꾸기
    - 클래스의 인스턴스를 만들때 생성자를 외부에서 바로 호출하는 것이
      아니라 캡슐화
    - 팩 함수를 만들어 호출
    - 인스턴스를 만드는 로직 자체를 캡슐화 하고 외부에서는 이 함수를 사용

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,17 +1,33 @@
+// 생성자 캡슐화 하기
 export class Employee {
+  // JS는 생성자를 private으로 만들 수 없음
   constructor(name, typeCode) {
-    this._name = name;
-    this._typeCode = typeCode;
+    this._name = name
+    this._typeCode = typeCode
   }
   get name() {
-    return this._name;
+    return this._name
   }
 
   get type() {
-    return Employee.legalTypeCodes[this._typeCode];
+    return Employee.legalTypeCodes[this._typeCode]
   }
 
   static get legalTypeCodes() {
-    return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
+    return { E: 'Engineer', M: 'Manager', S: 'Salesman' }
+  }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E')
+  }
+
+  static createSeniorEngineer(name) {
+    return new Employee(name, 'SE')
+  }
+
+  static createMarketer(name) {
+    return new Employee(name, 'M')
   }
 }
+
+const employee = Employee.createEngineer('JH')
