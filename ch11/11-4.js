f41c743bedaf684e727c20e2366347c838cfd41a
jihye <wisdom_u@naver.com>
2023-01-16 14:34:53 +0900

    11.5 객체 통째로 넘기기

--- ch11/11-4.js
+++ ch11/11-4.js
@@ -1,22 +1,21 @@
 export function temperatureAlerts(room, plan) {
-  const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
-    alerts.push('room temperature went outside range');
+  const alerts = []
+  if (!plan.withinRange(room.daysTempRange)) {
+    alerts.push('room temperature went outside range')
   }
 
-  return alerts;
+  return alerts
 }
 
 export class HeatingPlan {
   constructor(temperatureRange) {
-    this._temperatureRange = temperatureRange;
+    this._temperatureRange = temperatureRange
   }
 
-  withinRange(bottom, top) {
+  withinRange(range) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
-    );
+      range.bottom >= this._temperatureRange.low &&
+      range.top <= this._temperatureRange.high
+    )
   }
 }
