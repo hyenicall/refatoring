82cc60d172c2a6660a718aec19e6b3142d46f505
jihye <wisdom_u@naver.com>
2023-01-16 17:59:33 +0900

    11.13 오코드 예외로 바꾸기
    - 리턴 할  때 알수없는 숫자를 쓰지말고
    - 에러를 상속할 수 있는 클래스를 만들어서 처리하면 됨

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,19 @@
 function localShippingRules(data) {
-  if (data) return new ShippingRules(data);
-  else return -23;
+  if (data) return new ShippingRules(data)
+  else throw new OrderProcessionError(-23)
+}
+
+class OrderProcessionError extends Error {
+  constructor(errorCode) {
+    super()
+    this.errorCode = errorCode
+  }
+}
+
+try {
+  const result = localShippingRules()
+} catch (error) {
+  if (error instanceof OrderProcessionError) {
+    console.log(error)
+  }
 }
