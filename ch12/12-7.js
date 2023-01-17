75964f2d0b7e8b5e3aa93931c5a69f193659b979
jihye <wisdom_u@naver.com>
2023-01-17 19:51:12 +0900

    12.8 서브클래스 제거하기

--- ch12/12-7.js
+++ ch12/12-7.js
@@ -1,53 +1,59 @@
 class Person {
-  #name;
-  constructor(name) {
-    this.#name = name;
+  #name
+  #genderCode
+  constructor(name, genderCode) {
+    this.#name = name
+    this.#genderCode = genderCode
   }
 
   get name() {
-    return this.#name;
+    return this.#name
   }
 
   get genderCode() {
-    return 'X';
+    return this.#genderCode
+  }
+
+  get isMale() {
+    return this.#genderCode === 'M'
+  }
+
+  static create(record) {
+    switch (record.gender) {
+      case 'M':
+        return new Person(record.name, 'M')
+      case 'F':
+        return new Person(record.name, 'F')
+      default:
+        return new Person(record.name, 'X')
+    }
   }
 }
 
 class Male extends Person {
   get genderCode() {
-    return 'M';
+    return 'M'
   }
 }
 
 class Female extends Person {
   get genderCode() {
-    return 'F';
+    return 'F'
   }
 }
 
 function loadFromInput(data) {
-  const result = [];
+  const result = []
   data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
-    result.push(person);
-  });
-  return result;
+    result.push(Person.create(record))
+  })
+  return result
 }
 
 const people = loadFromInput([
   { name: '엘리', gender: 'F' },
   { name: '철수', gender: 'M' },
   { name: '밥', gender: 'M' },
-]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
-console.log(numberOfMales);
+])
+const numberOfMales = people.filter((p) => p.isMale).length
+console.log(numberOfMales)
