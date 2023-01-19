94b49d5d2f82691f4af1b1402c3ec8f45cc3fd48
jihye <wisdom_u@naver.com>
2023-01-19 16:55:00 +0900

    13.4 로우레벨 리팩토링 하기2

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,27 +1,32 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0
-  let volumeCredits = 0
+  return renderPlainText(invoice, plays)
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5)
-
-    // 청구 내역을 출력한다.
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`
-    totalAmount += amountFor(perf)
   }
-  result += `총액: ${format(totalAmount / 100)}\n`
-  result += `적립 포인트: ${volumeCredits}점\n`
+
+  result += `총액: ${usd(totalAmount() / 100)}\n`
+  result += `적립 포인트: ${totalCredits()}점\n`
   return result
 
   function playFor(performance) {
     return plays[performance.playID]
+  }
+
+  function creditsFor(performance) {
+    let result = 0
+    result += Math.max(performance.audience - 30, 0)
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === playFor(performance).type) {
+      result += Math.floor(performance.audience / 5)
+    }
+    return result
   }
 
   function amountFor(performance) {
@@ -45,9 +50,17 @@ export function statement(invoice, plays) {
     }
     return result
   }
+
+  function totalAmount() {
+    return invoice.performances.reduce((sum, p) => (sum += amountFor(p)), 0)
+  }
+
+  function totalCredits() {
+    return invoice.performances.reduce((sum, p) => (sum += creditsFor(p)), 0)
+  }
 }
 
-function format(number) {
+function usd(number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
