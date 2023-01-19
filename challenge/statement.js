f17133ea3e765eebf71f1313481cff1da4a3659a
jihye <wisdom_u@naver.com>
2023-01-19 19:22:03 +0900

    13.8 새로운 기능 + 테스트 추가

--- challenge/statement.js
+++ challenge/statement.js
@@ -5,8 +5,26 @@
 import { createStatement } from './create_statement'
 
 export function statement(invoice, plays) {
-  const statement = createStatement(invoice, plays)
-  return renderPlainText(statement)
+  return renderPlainText(createStatement(invoice, plays))
+}
+
+export function htmlStatement(invoice, plays) {
+  return renderHTML(createStatement(invoice, plays))
+}
+
+function renderHTML(statement) {
+  let result = `<h1>청구 내역 (고객명: ${statement.customer})</h1>`
+  result += '<table>\n'
+  result += '<tr><th>play</th><th>석</th><th>cost</th></tr>'
+
+  for (let perf of statement.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`
+    result += `<td>${usd(perf.amount / 100)}</td></tr>\n`
+  }
+  result += '</table>\n'
+  result += `<p>총액: <em>${usd(statement.totalAmount / 100)}</em></p>\n`
+  result += `<p>적립 포인트: <em>${statement.totalCredits}</em>점</p>\n`
+  return result
 }
 
 function renderPlainText(statement) {
@@ -59,6 +77,7 @@ const invoicesJSON = [
 ]
 
 const result = statement(invoicesJSON[0], playsJSON)
+
 const expected =
   '청구 내역 (고객명: BigCo)\n' +
   '  Hamlet: $650.00 (55석)\n' +
--- challenge/tests/statement.test.js
+++ challenge/tests/statement.test.js
@@ -1,11 +1,11 @@
-import { statement } from '../statement.js';
+import { htmlStatement, statement } from '../statement.js'
 
 describe('statement', () => {
   const playsJSON = {
     hamlet: { name: 'Hamlet', type: 'tragedy' },
     'as-like': { name: 'As You Like It', type: 'comedy' },
     othello: { name: 'Othello', type: 'tragedy' },
-  };
+  }
 
   const invoicesJSON = [
     {
@@ -25,7 +25,7 @@ describe('statement', () => {
         },
       ],
     },
-  ];
+  ]
   it('should print a statement', () => {
     const expected =
       '청구 내역 (고객명: BigCo)\n' +
@@ -33,19 +33,21 @@ describe('statement', () => {
       '  As You Like It: $580.00 (35석)\n' +
       '  Othello: $500.00 (40석)\n' +
       '총액: $1,730.00\n' +
-      '적립 포인트: 47점\n';
+      '적립 포인트: 47점\n'
 
-    expect(statement(invoicesJSON[0], playsJSON)).toBe(expected);
-  });
-});
+    expect(statement(invoicesJSON[0], playsJSON)).toBe(expected)
+  })
 
-{
-  /* <h1>청구 내역 (고객명: BigCo)</h1>
-      <table>
-      <tr><th>play</th><th>석</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
-        <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
-        <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
-      </table>
-      <p>총액: <em>$1,730.00</em></p>
-      <p>적립 포인트: <em>47</em>점</p> */
-}
+  it('should print a statement in HTML format', () => {
+    let result = `<h1>청구 내역 (고객명: BigCo)</h1>`
+    result += '<table>\n'
+    result += `<tr><th>play</th><th>석</th><th>cost</th></tr><tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n`
+    result += `<tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n`
+    result += `<tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n`
+    result += '</table>\n'
+    result += `<p>총액: <em>$1,730.00</em></p>\n`
+    result += `<p>적립 포인트: <em>47</em>점</p>\n`
+
+    expect(htmlStatement(invoicesJSON[0], playsJSON)).toBe(result)
+  })
+})
