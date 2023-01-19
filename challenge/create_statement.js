d1b71a3b83e2db860a0a89564763a21d11414494
jihye <wisdom_u@naver.com>
2023-01-19 19:51:08 +0900

    13.10 다형성 이용하기

--- challenge/create_statement.js
+++ challenge/create_statement.js
@@ -14,44 +14,50 @@ class Performance {
     return this.#audience
   }
 
-  get credit() {
-    let result = 0
-    result += Math.max(this.#audience - 30, 0)
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === this.#play.type) {
-      result += Math.floor(this.#audience / 5)
+  static create(audience, play) {
+    switch (play.type) {
+      case 'tragedy':
+        return new Tragedy(audience, play)
+      case 'comedy':
+        return new Comedy(audience, play)
+      default:
+        throw new Error(`알수없는 타입: ${play.type}`)
     }
+  }
+}
+
+class Tragedy extends Performance {
+  get amount() {
+    const base = 40000
+    return this.audience > 30 ? base + 1000 * (this.audience - 30) : base
+  }
+
+  get credits() {
+    return Math.max(this.audience - 30, 0)
+  }
+}
+
+class Comedy extends Performance {
+  get amount() {
+    let result = 30000
+    if (this.audience > 20) {
+      result += 10000 + 500 * (this.audience - 20)
+    }
+    result += 300 * this.audience
+
     return result
   }
 
-  get amount() {
-    let result = 0
-    switch (this.#play.type) {
-      case 'tragedy': // 비극
-        result = 40000
-        if (this.#audience > 30) {
-          result += 1000 * (this.#audience - 30)
-        }
-        break
-      case 'comedy': // 희극
-        result = 30000
-        if (this.#audience > 20) {
-          result += 10000 + 500 * (this.#audience - 20)
-        }
-        result += 300 * this.#audience
-        break
-      default:
-        throw new Error(`알 수 없는 장르: ${this.#play.type}`)
-    }
-    return result
+  get credits() {
+    return Math.max(this.audience - 30, 0) + Math.floor(this.audience / 5)
   }
 }
 
 export function createStatement(invoice, plays) {
   const statement = {}
   statement.customer = invoice.customer
-  statement.performances = invoice.performances.map(
-    (p) => new Performance(p.audience, plays[p.playID])
+  statement.performances = invoice.performances.map((p) =>
+    Performance.create(p.audience, plays[p.playID])
   )
   statement.totalAmount = totalAmount(statement.performances)
   statement.totalCredits = totalCredits(statement.performances)
@@ -63,6 +69,6 @@ export function createStatement(invoice, plays) {
   }
 
   function totalCredits(performances) {
-    return performances.reduce((sum, p) => (sum += p.credit), 0)
+    return performances.reduce((sum, p) => (sum += p.credits), 0)
   }
 }
