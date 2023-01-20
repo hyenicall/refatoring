8c374e5f8c8f2187b5375a7e94b023f1e444d1a6
jihye <wisdom_u@naver.com>
2023-01-20 15:33:57 +0900

    14.1 ~ 14.4 성공하는 테스트 완성하기

---
+++ ch6-testable/test/6-1.js
@@ -0,0 +1,37 @@
+export function printOwing(invoice, console, clock) {
+  let outstanding = 0
+
+  console.log('***********************')
+  console.log('**** Customer Owes ****')
+  console.log('***********************')
+
+  // calculate outstanding
+  for (const o of invoice.orders) {
+    outstanding += o.amount
+  }
+
+  // record due date
+  // const today = new Date()
+  const today = clock.today
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  )
+
+  //print details
+  console.log(`name: ${invoice.customer}`)
+  console.log(`amount: ${outstanding}`)
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
+}
+class Clock {
+  constructor() {}
+  get today() {
+    return new Date()
+  }
+}
+const invoice = {
+  orders: [{ amount: 2 }, { amount: 5 }],
+  customer: '엘리',
+}
+printOwing(invoice, console, new Clock())
